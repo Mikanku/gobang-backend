dec504085b9ace7c7b72e073712a4cf8cf176c03
Mikanku <liveincode@163.com>
2025-06-24 08:21:45 +0800

    feat: init

---
+++ src/main/java/icu/mikanku/gobangbackend/GobangBackendApplication.java
@@ -0,0 +1,13 @@
+package icu.mikanku.gobangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GobangBackendApplication.class, args);
+    }
+
+}
