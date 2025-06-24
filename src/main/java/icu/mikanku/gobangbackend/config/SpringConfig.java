79f1f953f4ee2ddff5d75d44f64d1cc61058e11e
Mikanku <liveincode@163.com>
2025-06-24 08:22:55 +0800

    feat: config init

---
+++ src/main/java/icu/mikanku/gobangbackend/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package icu.mikanku.gobangbackend.config;
+
+import cn.hutool.extra.spring.SpringUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+@Import(SpringUtil.class)
+@Configuration
+public class SpringConfig {
+
+}
